fd119702cde9fd8dbce1a4540a0fe35837dfed6a
AdrianoR85 <adrianorosa085@gmail.com>
2024-03-07 00:41:40 -0300

    add touch

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import TaskProvider from './context/TasksContext'
 import { Toaster } from 'react-hot-toast';
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from 'react-dnd-touch-backend';
 import './styles/app.css'
+
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+const Backend = isMobile ? TouchBackend : HTML5Backend;
 function App(){
   return (
     <TaskProvider>
-      <DndProvider backend={HTML5Backend}>
+      <DndProvider backend={Backend}>
         <div className='app'>      
             <Toaster />
               <header>
